4457372b4a682c2ce8709d5fd2a2d80fc7c7b73e
Pratick Baraik <animeNerd.int@protonmail.com>
2026-03-20 23:16:48 +0530

    no audio provider now

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
-import { AudioProvider } from "../context/AudioContext";
 
 import Router from "./router";
 import ScrollRestoration from "./scrollRestoration"; // 👈 ADD
 
 export default function App() {
   return (
-    <AudioProvider>
-      <BrowserRouter>
-        <ScrollRestoration /> {/* 👈 MUST be inside BrowserRouter */}
-        <Router />
-      </BrowserRouter>
-    </AudioProvider>
+    // <AudioProvider>
+    <BrowserRouter>
+      <ScrollRestoration /> {/* 👈 MUST be inside BrowserRouter */}
+      <Router />
+    </BrowserRouter>
+    // </AudioProvider>
   );
 }
